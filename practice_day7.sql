9f6000237f370b9b2ad9290467bb9f318c0a800b
vihuong03 <160115341+vihuong03@users.noreply.github.com>
2024-02-21 14:16:53 +0700

    Update practice_day7.sql

--- practice_day7.sql
+++ practice_day7.sql
@@ -8,3 +8,38 @@ select user_id,
 concat(upper(left(name,1)),'',lower(right(name, length(name)-1))) as name
 from Users
 --- bai 3
+SELECT manufacturer,
+'$' || round(sum(total_sales)/1000000) || ' ' || 'million' as sale
+FROM pharmacy_sales
+group by manufacturer
+order by sum(total_sales) DESC
+--- bai 4
+SELECT extract(month from submit_date) as month,
+product_id as product,
+round(avg(stars),2) as avg_stars
+FROM reviews
+group by extract(month from submit_date), product_id
+order by month, product_id
+--- bai 5
+SELECT sender_id,
+count(message_id) as message_count
+FROM messages
+where extract(month from sent_date)=08 and extract(year from sent_date)=2022
+group by sender_id
+order by count(message_id) DESC
+limit 2
+--- bai 6
+select tweet_id
+from Tweets
+where length(content) >15
+--- bai 7
+
+--- bai 8
+select count(id)
+from employees
+where joining_date between '2022-01-01' and '2022-08-01'
+--- bai 9
+select position('a' in first_name)
+from worker
+where first_name = 'Amitah'
+--- bai10 
