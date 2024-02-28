1c55d483809230264464d6a27eaae7badde83b6e
vihuong03 <160115341+vihuong03@users.noreply.github.com>
2024-02-28 17:31:25 +0700

    Update practice_day13.sql

--- practice_day13.sql
+++ practice_day13.sql
@@ -48,6 +48,7 @@ HAVING COUNT(DISTINCT EXTRACT(MONTH FROM event_date)) = 2)
 select count(distinct user_id)
 from cte
 ---bai6 
+/* CACH MIINH LAM SAI RUI
 with cte_1 as (
 select extract(month from trans_date) as month,
 country, sum(amount) as trans_total_amount,count(*) as trans_count
@@ -62,7 +63,14 @@ group by extract(month from trans_date),country )
 select a.month, a.country, a.trans_count,b.approved_count, a.trans_total_amount, b.approved_total_amount
 from cte_1 as a
 join cte_2 as b on a.country=b.country
-group by a.month, a.country
+group by a.month, a.country */
+/* CHỮA
+select extract(month from trans_date) as month, country, count(*) as transcount,
+sum(case when state='approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state= 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by month, country
 ---bai 7
 with cte as(
 select product_id, year, quantity, price,
@@ -73,16 +81,29 @@ select product_id, year as first_year, quantity, price
 from cte 
 where xephang =1
 ---bai 8
+/* sai vì có trg hợp có nhiều hơn 2 product key mà mình ko đếm dc
 select a.customer_id
 from Customer as a
 join Product as b on a.product_key=b.product_key
 group by customer_id
 having count(distinct b.product_key)=2
+/* chữa
+select a.customer_id
+from Customer as a
+join Product as b on a.product_key=b.product_key
+group by customer_id
+having count(b.product_key)=(select count(distinct product_key) from product)
 ---bai 9
+/* sai roi
 select b.employee_id
 from Employees a, Employees b
 where a.manager_id=b.employee_id
 and b.salary<30000
+/* chua
+select employee_id
+from Employees
+where manager_id not in (select employee_id from Employees)
+and salary<30000
 --- bai 10
 with cte as(
 SELECT company_id,title,description,
@@ -93,6 +114,7 @@ select count(distinct company_id) as duplicate
 from cte 
 where count=2
 ---bai 11
+/* saiiii
 with cte as
 ((select b.name,
 count(rating)
@@ -112,5 +134,35 @@ having avg(b.rating)>=3.5
 order by title asc
 limit 1))
 select name as results from cte
+/* CHUA
+with cte as
+((select b.name,
+count(rating)
+from MovieRating as a
+join Users as b on a.user_id=b.user_id
+group by b.name
+order by name asc
+limit 1)
+union all
+(select a.title,
+avg(b.rating)
+from Movies a
+join MovieRating b on a.movie_id=b.movie_id
+where extract(month from created_at)=02 and extract(year from created_at)=2020
+group by a.title
+order by avg(b.rating) desc, title asc
+limit 1))
+select name as results from cte
 ---bai 12
+with cte as
+(select requester_id as id from RequestAccepted
+union all
+select accepter_id as id from RequestAccepted)
+select id, count(*) as num
+from cte
+group by id
+order by num desc
+limit 1
+
+
 
